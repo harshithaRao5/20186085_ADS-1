da086d3f3e40dd8f9182538ab721c8785e24bcd8
harshithaRao5 <harshithanallani@msitprogram.net>
2018-09-25 20:03:53 +0530

    "check_style_score -> 24 of 24 passed. style score: 1/1 "

--- ADS1-assignment/m2/CodeCamp_Percolation/CodeCamp_Percolation/Solution.java
+++ ADS1-assignment/m2/CodeCamp_Percolation/CodeCamp_Percolation/Solution.java
@@ -1,10 +1,16 @@
 // public class Percolation {
-//    public Percolation(int n)                // create n-by-n grid, with all sites blocked
-//    public    void open(int row, int col)    // open site (row, col) if it is not open already
-//    public boolean isOpen(int row, int col)  // is site (row, col) open?
-//    public boolean isFull(int row, int col)  // is site (row, col) full?
-//    public     int numberOfOpenSites()       // number of open sites
-//    public boolean percolates()              // does the system percolate?
+//    public Percolation(int n)
+// create n-by-n grid, with all sites blocked
+//    public    void open(int row, int col)
+// open site (row, col) if it is not open already
+//    public boolean isOpen(int row, int col)
+// is site (row, col) open?
+//    public boolean isFull(int row, int col)
+// is site (row, col) full?
+//    public     int numberOfOpenSites()
+// number of open sites
+//    public boolean percolates()
+// does the system percolate?
 // }
 
 
@@ -195,8 +201,8 @@ public final class Solution {
         Percolation pobj = new Percolation(n);
         while (scan.hasNext()) {
             String[] tokens = scan.nextLine().split(" ");
-            pobj.open(Integer.parseInt(tokens[0])
-                , Integer.parseInt(tokens[1]));
+            pobj.open(Integer.parseInt(tokens[0]),
+            Integer.parseInt(tokens[1]));
         }
         System.out.println(pobj.percolates()
             && pobj.numberOfOpenSites() != 0);
