379e764980b9d1cccb506d0dc50cc4e80624cfba
harshithaRao5 <harshithanallani@msitprogram.net>
2018-09-26 15:10:33 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- ADS1-assignment/m3/3-sum/3-sum/Solution.java
+++ ADS1-assignment/m3/3-sum/3-sum/Solution.java
@@ -17,14 +17,10 @@ class ThreeSum {
     }
 
     public static int count(int[] a) {
-        int n = a.length;
         Arrays.sort(a);
-        if (containsDuplicates(a)) {
-            throw new IllegalArgumentException("array contains duplicate integers");
-        }
         int count = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = i+1; j < n; j++) {
+        for (int i = 0; i < a.length; i++) {
+            for (int j = i+1; j < a.length; j++) {
                 int k = Arrays.binarySearch(a, -(a[i] + a[j]));
                 if (k > j) {
                     count++;
