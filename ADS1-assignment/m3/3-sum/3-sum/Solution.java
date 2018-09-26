45c06182a4196ac7a9ac4215f3e7c78d15765251
harshithaRao5 <harshithanallani@msitprogram.net>
2018-09-26 19:41:25 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- ADS1-assignment/m3/3-sum/3-sum/Solution.java
+++ ADS1-assignment/m3/3-sum/3-sum/Solution.java
@@ -1,22 +1,36 @@
 import java.util.Scanner;
 import java.util.Arrays;
-
+/**
+ * Class for three sum.
+ */
 class ThreeSum {
+    /**
+     * integer array.
+     */
     int[] array;
+    /**
+     * array size
+     */
     int size;
+    /**
+     * Constructs the object.
+     *
+     * @param      arr   The arr
+     * @param      n     integer
+     */
     ThreeSum(int[] arr, int n) {
         this.array = arr;
         this.size = n;
     }
-    private boolean containsDuplicates(int[] a) {
-        for (int i = 1; i < a.length; i++) {
-            if (a[i] == a[i-1]) {
-                return true;
-            }
-        }
-        return false;
-    }
-    public int binarySearch(int[] array, int a) {
+   /**
+    * binary search method.
+    *
+    * @param      array  The array
+    * @param      a      integer
+    *
+    * @return result
+    */
+    public int binarySearch(final int[] array, final int a) {
         int low = 0;
         int high = array.length - 1;
         while (low <= high) {
@@ -31,13 +45,22 @@ class ThreeSum {
         }
         return -1;
     }
-    public int count(int[] a) {
+    /**
+     * count of sum.
+     *
+     * @param      a integer
+     *
+     * @return  count
+     */
+    public int count(final int[] a) {
         Arrays.sort(a);
+        // System.out.println(Arrays.toString(a));
         int count = 0;
         for (int i = 0; i < a.length; i++) {
             for (int j = i+1; j < a.length; j++) {
                 int result = binarySearch(a, -(a[i] + a[j]));
-                if (result > j && result > 0) {
+                //System.out.println(result + "result$$$$j" + j);
+                if (result > j) {
                     count++;
                 }
             }
@@ -45,12 +68,22 @@ class ThreeSum {
         return count;
     }
 }
+/**
+ * Class for solution.
+ */
 public class Solution {
-
+/**
+ * Constructs the object.
+ */
     private Solution() {
 
     }
-    public static void main(String[] args) {
+    /**
+     * main function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
         int[] array = new int[n];
