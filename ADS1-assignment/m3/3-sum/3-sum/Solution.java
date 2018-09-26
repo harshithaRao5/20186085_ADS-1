ad010a606fb8414d37c2b80921f76d01a9352db1
harshithaRao5 <harshithanallani@msitprogram.net>
2018-09-26 17:23:43 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- ADS1-assignment/m3/3-sum/3-sum/Solution.java
+++ ADS1-assignment/m3/3-sum/3-sum/Solution.java
@@ -2,11 +2,12 @@ import java.util.Scanner;
 import java.util.Arrays;
 
 class ThreeSum {
-
-    ThreeSum() {
-
-     }
-
+    int[] array;
+    int size;
+    ThreeSum(int[] arr, int n) {
+        this.array = arr;
+        this.size = n;
+    }
     private static boolean containsDuplicates(int[] a) {
         for (int i = 1; i < a.length; i++) {
             if (a[i] == a[i-1]) {
@@ -15,14 +16,28 @@ class ThreeSum {
         }
         return false;
     }
-
+    public static int binarySearch(int[] array, int a) {
+        int low = 0;
+        int high = array.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (array[mid] == a) {
+                return a;
+            } else if (array[mid] < a) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return 1;
+    }
     public static int count(int[] a) {
         Arrays.sort(a);
         int count = 0;
         for (int i = 0; i < a.length; i++) {
             for (int j = i+1; j < a.length; j++) {
-                int k = Arrays.binarySearch(a, -(a[i] + a[j]));
-                if (k > j) {
+                int result = binarySearch(a, -(a[i] + a[j]));
+                if (result > j) {
                     count++;
                 }
             }
@@ -39,7 +54,10 @@ public class Solution {
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
         int[] array = new int[n];
-        ThreeSum tsum = new ThreeSum();
+        for (int i = 0; i < n; i++) {
+            array[i] = scan.nextInt();
+        }
+        ThreeSum tsum = new ThreeSum(array, n);
         System.out.println(tsum.count(array));
     }
 }
