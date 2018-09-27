5d72d691edc39d077c4d66196df50f0dd32e9981
harshithaRao5 <harshithanallani@msitprogram.net>
2018-09-27 20:25:23 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- ADS1-assignment/m4/4.1 Balanced Paranthesis/4.1 Balanced Paranthesis/Solution.java
+++ ADS1-assignment/m4/4.1 Balanced Paranthesis/4.1 Balanced Paranthesis/Solution.java
@@ -1,30 +1,79 @@
 import java.util.Scanner;
+/**
+ * Class for linked stack of strings.
+ */
 class LinkedStackOfStrings {
+	/**
+	 * variable.
+	 */
 	private Node first = null;
+	/**
+	 * Class for node.
+	 */
 	private class Node {
+		/**
+		 * variable.
+		 */
 		char item;
+		/**
+		 * variable.
+		 */
 		Node nextAddress;
 	}
+	/**
+	 * top element of stack.
+	 *
+	 * @return    first element
+	 */
 	public char top() {
 		return first.item;
 	}
+	/**
+	 * Determines if empty.
+	 *
+	 * @return     True if empty, False otherwise.
+	 */
 	public boolean isEmpty() {
 		return first == null;
 	}
-	public void push(char item) {
+	/**
+	 * add element to stack.
+	 *
+	 * @param      item  The item
+	 */
+	public void push(final char item) {
 		Node oldfirst = first;
 		first = new Node();
 		first.item = item;
 		first.nextAddress = oldfirst;
 	}
+	/**
+	 * element deleted element.
+	 *
+	 * @return deleted element.
+	 */
 	public char pop() {
 		char item = first.item;
 		first = first.nextAddress;
 		return item;
 	}
 }
-public class Solution {
-	public static void main(String[] args) {
+/**
+ * main method
+ */
+public final class Solution {
+	/**
+	 * Constructs the object.
+	 */
+	private Solution() {
+
+	}
+	/**
+	 *
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int size = Integer.parseInt(sc.nextLine());
 		for (int i = 0; i < size; i++) {
@@ -37,7 +86,14 @@ public class Solution {
 
 		}
 	}
-	public static boolean checkParanthesis(String s) {
+	/**
+	 *check paranthesis method.
+	 *
+	 * @param      s     string.
+	 *
+	 * @return     boolean.
+	 */
+	public static boolean checkParanthesis(final String s) {
 		LinkedStackOfStrings obj = new LinkedStackOfStrings();
 		int sLength = s.length();
 		for (int i = 0; i < sLength; i++) {
