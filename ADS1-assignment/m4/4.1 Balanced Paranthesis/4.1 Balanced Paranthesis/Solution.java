6aea6688ef5e38cb724426fdf5085fbbbfc4e546
harshithaRao5 <harshithanallani@msitprogram.net>
2018-09-27 20:28:40 +0530

    "check_style_score -> 4 of 4 passed. style score: 1/1 "

--- ADS1-assignment/m4/4.1 Balanced Paranthesis/4.1 Balanced Paranthesis/Solution.java
+++ ADS1-assignment/m4/4.1 Balanced Paranthesis/4.1 Balanced Paranthesis/Solution.java
@@ -3,118 +3,118 @@ import java.util.Scanner;
  * Class for linked stack of strings.
  */
 class LinkedStackOfStrings {
-	/**
-	 * variable.
-	 */
-	private Node first = null;
-	/**
-	 * Class for node.
-	 */
-	private class Node {
-		/**
-		 * variable.
-		 */
-		char item;
-		/**
-		 * variable.
-		 */
-		Node nextAddress;
-	}
-	/**
-	 * top element of stack.
-	 *
-	 * @return    first element
-	 */
-	public char top() {
-		return first.item;
-	}
-	/**
-	 * Determines if empty.
-	 *
-	 * @return     True if empty, False otherwise.
-	 */
-	public boolean isEmpty() {
-		return first == null;
-	}
-	/**
-	 * add element to stack.
-	 *
-	 * @param      item  The item
-	 */
-	public void push(final char item) {
-		Node oldfirst = first;
-		first = new Node();
-		first.item = item;
-		first.nextAddress = oldfirst;
-	}
-	/**
-	 * element deleted element.
-	 *
-	 * @return deleted element.
-	 */
-	public char pop() {
-		char item = first.item;
-		first = first.nextAddress;
-		return item;
-	}
+    /**
+     * variable.
+     */
+    private Node first = null;
+    /**
+     * Class for node.
+     */
+    private class Node {
+        /**
+         * variable.
+         */
+        private char item;
+        /**
+         * variable.
+         */
+        private Node nextAddress;
+    }
+    /**
+     * top element of stack.
+     *
+     * @return    first element
+     */
+    public char top() {
+        return first.item;
+    }
+    /**
+     * Determines if empty.
+     *
+     * @return     True if empty, False otherwise.
+     */
+    public boolean isEmpty() {
+        return first == null;
+    }
+    /**
+     * add element to stack.
+     *
+     * @param      item  The item
+     */
+    public void push(final char item) {
+        Node oldfirst = first;
+        first = new Node();
+        first.item = item;
+        first.nextAddress = oldfirst;
+    }
+    /**
+     * element deleted element.
+     *
+     * @return deleted element.
+     */
+    public char pop() {
+        char item = first.item;
+        first = first.nextAddress;
+        return item;
+    }
 }
 /**
- * main method
+ * main method.
  */
 public final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
 
-	}
-	/**
-	 *
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int size = Integer.parseInt(sc.nextLine());
-		for (int i = 0; i < size; i++) {
-			String line = sc.next();
-			if (checkParanthesis(line)) {
-				System.out.println("YES");
-			} else {
-				System.out.println("NO");
-			}
+    }
+    /**
+     *
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = Integer.parseInt(sc.nextLine());
+        for (int i = 0; i < size; i++) {
+            String line = sc.next();
+            if (checkParanthesis(line)) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
 
-		}
-	}
-	/**
-	 *check paranthesis method.
-	 *
-	 * @param      s     string.
-	 *
-	 * @return     boolean.
-	 */
-	public static boolean checkParanthesis(final String s) {
-		LinkedStackOfStrings obj = new LinkedStackOfStrings();
-		int sLength = s.length();
-		for (int i = 0; i < sLength; i++) {
-			char ch = s.charAt(i);
-			if (ch == '(' || ch == '{' || ch == '[') {
-				obj.push(ch);
-			} else {
-				if (obj.isEmpty()) {
-					return false;
-				} else if (ch == ')' && obj.top() == '(') {
-					obj.pop();
-				} else if (ch == '}' && obj.top() == '{') {
-					obj.pop();
-				} else if (ch == ']' && obj.top() == '[') {
-					obj.pop();
-				} else {
-					return false;
-				}
-			}
+        }
+    }
+    /**
+     *check paranthesis method.
+     *
+     * @param      s     string.
+     *
+     * @return     boolean.
+     */
+    public static boolean checkParanthesis(final String s) {
+        LinkedStackOfStrings obj = new LinkedStackOfStrings();
+        int sLength = s.length();
+        for (int i = 0; i < sLength; i++) {
+            char ch = s.charAt(i);
+            if (ch == '(' || ch == '{' || ch == '[') {
+                obj.push(ch);
+            } else {
+                if (obj.isEmpty()) {
+                    return false;
+                } else if (ch == ')' && obj.top() == '(') {
+                    obj.pop();
+                } else if (ch == '}' && obj.top() == '{') {
+                    obj.pop();
+                } else if (ch == ']' && obj.top() == '[') {
+                    obj.pop();
+                } else {
+                    return false;
+                }
+            }
 
-		}
-		return obj.isEmpty();
-	}
+        }
+        return obj.isEmpty();
+    }
 }
