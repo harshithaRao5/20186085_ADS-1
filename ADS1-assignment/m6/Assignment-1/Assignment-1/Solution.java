a6d5a128662c98d350a4fe7d7eaa8bb46b89d858
harshithaRao5 <harshithanallani@msitprogram.net>
2018-09-29 13:05:16 +0530

    "check_style_score -> 0 of 12 passed. style score: 0/1 "

--- ADS1-assignment/m6/Assignment-1/Assignment-1/Solution.java
+++ ADS1-assignment/m6/Assignment-1/Assignment-1/Solution.java
@@ -1,126 +1,89 @@
 import java.util.Scanner;
-class LinkedList<Item> {
-    private stack<Item> first;     // top of stack
-    private int n;                // size of the stack
-
-    // helper linked list class
-    private static class stack<Item> {
-        private Item item;
-        private stack<Item> next;
-    }
-
-    /**
-     * Initializes an empty stack.
-     */
+class LinkedList {
+    private LinkedList first;
+    private LinkedList last;
+    private LinkedList next;
+    private char item;
+    private int length;
     public LinkedList() {
         first = null;
-        n = 0;
+        last = null;
+        length = 0;
     }
 
-    /**
-     * Returns true if this stack is empty.
-     *
-     * @return true if this stack is empty; false otherwise
-     */
     public boolean isEmpty() {
         return first == null;
     }
+    public void push(char item) {
+        LinkedList oldlast = last;
+        last = new LinkedList();
+        last.item = item;
+        last.next = null;
+        if (isEmpty()) {
+            first = last;
+        } else {
+            oldlast.next = last;
+        }
+        length++;
+    }
 
-    /**
-     * Returns the number of items in this stack.
-     *
-     * @return the number of items in this stack
-     */
+    public char pop() {
+        char item = first.item;
+        if (first != null) {
+            first = first.next;
+            length--;
+        }
+        return item;
+    }
     public int size() {
-        return n;
+        System.out.println(length);
+        return length;
+    }
+}
+class AddLargeNumbers {
+    private static LinkedList lobj = new LinkedList();
+
+    public static LinkedList numberToDigits(String number) {
+        for (int i = 0; i < number.length(); i++) {
+            lobj.push(number.charAt(i));
+        }
+        return lobj;
+    }
+    public static String digitsToNumber(LinkedList list) {
+        String s = "";
+        for (int i = 0; i <= list.size(); i++) {
+            s += lobj.pop();
+        }
+        return s;
+
     }
 
-    /**
-     * Adds the item to this stack.
-     *
-     * @param  item the item to add
-     */
-    public void push(Item item) {
-        stack<Item> oldfirst = first;
-        first = new stack<Item>();
-        first.item = item;
-        first.next = oldfirst;
-        n++;
-    }
-
-    /**
-     * Removes and returns the item most recently added to this stack.
-     *
-     * @return the item most recently added
-     * @throws NoSuchElementException if this stack is empty
-     */
-    public Item pop() {
-        Item item = null;
-        if (!isEmpty())
-            item = first.item;        // save item to return
-        first = first.next;            // delete first stack
-        n--;
-        return item;                   // return the saved item
-    }
-
-
-    /**
-     * Returns (but does not remove) the item most recently added to this stack.
-     *
-     * @return the item most recently added to this stack
-     * @throws NoSuchElementException if this stack is empty
-     */
-    public Item peek() {
-        if (!isEmpty())
-            return first.item;
+    public static LinkedList addLargeNumbers(LinkedList list1, LinkedList list2) {
         return null;
     }
 
 }
+    public class Solution {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            String input = sc.nextLine();
+            String p = sc.nextLine();
+            String q = sc.nextLine();
+            switch (input) {
+            case "numberToDigits":
+                LinkedList pDigits = AddLargeNumbers.numberToDigits(p);
+                LinkedList qDigits = AddLargeNumbers.numberToDigits(q);
+                System.out.println(AddLargeNumbers.digitsToNumber(pDigits));
+                System.out.println(AddLargeNumbers.digitsToNumber(qDigits));
+                break;
 
-class AddLargeNumbers {
-        private static LinkedList lobj = new LinkedList();
-        public static LinkedList numberToDigits(String number) {
-            for (int i = 0; i < number.length(); i++) {
-                lobj.push(number.charAt(i));
+            case "addLargeNumbers":
+                pDigits = AddLargeNumbers.numberToDigits(p);
+                qDigits = AddLargeNumbers.numberToDigits(q);
+                LinkedList result = AddLargeNumbers.addLargeNumbers(pDigits, qDigits);
+                System.out.println(AddLargeNumbers.digitsToNumber(result));
+                break;
             }
-            return lobj;
-        }
-        public static String digitsToNumber(LinkedList list) {
-            String s = "";
-            for (int i = 0; i < list.size(); i++) {
-                s += lobj.pop();
-            }
-            return s;
-
         }
 
-        public static LinkedList addLargeNumbers(LinkedList list1, LinkedList list2) {
-            return null;
-        }
     }
-
-public class Solution {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
-        String p = sc.nextLine();
-        String q = sc.nextLine();
-        switch (input) {
-        case "numberToDigits":
-            LinkedList pDigits = AddLargeNumbers.numberToDigits(p);
-            LinkedList qDigits = AddLargeNumbers.numberToDigits(q);
-            System.out.println(AddLargeNumbers.digitsToNumber(pDigits));
-            System.out.println(AddLargeNumbers.digitsToNumber(qDigits));
-            break;
-
-        case "addLargeNumbers":
-            pDigits = AddLargeNumbers.numberToDigits(p);
-            qDigits = AddLargeNumbers.numberToDigits(q);
-            LinkedList result = AddLargeNumbers.addLargeNumbers(pDigits, qDigits);
-            System.out.println(AddLargeNumbers.digitsToNumber(result));
-            break;
-        }
-    }
-
-}
