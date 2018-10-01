b200c61c206e648c3c7449c11d3457a9c57a93d9
harshithaRao5 <harshithanallani@msitprogram.net>
2018-10-01 15:43:31 +0530

    "check_style_score -> 0 of 2 passed. style score: 0/1 "

--- ADS1-assignment/m7/7.1 Steque/7.1 Steque/Solution.java
+++ ADS1-assignment/m7/7.1 Steque/7.1 Steque/Solution.java
@@ -151,9 +151,6 @@ public class Solution {
 			//System.out.println();
 			while (sc.hasNext()) {
 				String[] tokens = sc.nextLine().split(" ");
-				if (tokens[0].equals("")) {
-                    System.out.println();
-                }
 				switch(tokens[0]) {
 					case "push":
 					sobj.push(Integer.parseInt(tokens[1]));
@@ -171,7 +168,9 @@ public class Solution {
 					sobj = new Steque();
 					break;
 				}
-
+				if (tokens[0].equals("")) {
+                    System.out.println();
+                }
 			}
 		}
 	}
