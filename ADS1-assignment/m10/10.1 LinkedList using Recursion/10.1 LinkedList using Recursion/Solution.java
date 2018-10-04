254c9cd4173b67369e9d6d5e81974ba2dbe32b65
harshithaRao5 <harshithanallani@msitprogram.net>
2018-10-04 20:26:58 +0530

    "ads1_m10_a1: 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS1-assignment/m10/10.1 LinkedList using Recursion/10.1 LinkedList using Recursion/Solution.java
+++ ADS1-assignment/m10/10.1 LinkedList using Recursion/10.1 LinkedList using Recursion/Solution.java
@@ -1,35 +1,94 @@
 import java.util.Scanner;
+/**
+ * Class for node.
+ */
 class Node {
+	/**
+	 * variable declarartion.
+	 */
 	public int item;
+	/**
+	 * variable declarartion.
+	 */
 	public Node next;
-	Node(int data) {
+	/**
+	 * Constructs the object.
+	 *
+	 * @param      data  The data
+	 */
+	Node(final int data) {
 		item = data;
 		next = null;
 	}
+	/**
+	 * Gets the item.
+	 *
+	 * @return     The item.
+	 */
 	public int getItem() {
 		return item;
 	}
+	/**
+	 * Gets the next.
+	 *
+	 * @return     The next.
+	 */
 	public Node getNext() {
 		return next;
 	}
 }
+/**
+ * List of linkeds.
+ */
 class linkedList {
+	/**
+	 * variable declaration.
+	 */
 	private Node head;
+	/**
+	 * variable declaration.
+	 */
 	private int size;
+	/**
+	 * Constructs the object.
+	 */
 	linkedList() {
 		head = null;
 		size = 0;
 	}
+	/**
+	 *size of list.
+	 *
+	 * @return length.
+	 */
 	public int length() {
 		return size;
 	}
+	/**
+	 * Determines if empty.
+	 *
+	 * @return     True if empty, False otherwise.
+	 */
 	public boolean isEmpty() {
 		return head == null;
 	}
-	public void insertAt(int position, int item1) {
+	/**
+	 * insert at position.
+	 *
+	 * @param      position  The position
+	 * @param      item1     The item 1
+	 */
+	public void insertAt(final int position, final int item1) {
 		insertAt(head, position, item1);
 	}
-	private void insertAt(Node node, int position, int item1) {
+	/**
+	 * insert at position.
+	 *
+	 * @param      node      The node
+	 * @param      position  The position
+	 * @param      item1     The item 1
+	 */
+	private void insertAt(final Node node, final int position, final int item1) {
 		if (position == 0 && head == null) {
 			head = new Node(item1);
 		} else if (position == 0 && head != null) {
@@ -42,18 +101,26 @@ class linkedList {
 				temp = temp.getNext();
 			}
 			Node nextNode = temp.getNext();
-			node = new Node(item1);
-			temp.next = node;
-			node.next = nextNode;
+			Node node1 = new Node(item1);
+			temp.next = node1;
+			node1.next = nextNode;
 		}
 		size++;
 	}
-
-
+	/**
+	 * reverse of list.
+	 */
 	public void reverse() {
 		head = reverse(head);
 	}
-	private Node reverse(Node head) {
+	/**
+	 * reverse of list.
+	 *
+	 * @param      head  The head
+	 *
+	 * @return  node.
+	 */
+	private Node reverse(final Node head) {
 		if (head == null || head.next == null) {
 			return head;
 		}
@@ -62,6 +129,11 @@ class linkedList {
 		head.next = null;
 		return temp;
 	}
+	/**
+	 * Returns a string representation of the object.
+	 *
+	 * @return     String representation of the object.
+	 */
 	public String toString() {
 		String s = "";
 		if (size != 0) {
@@ -75,12 +147,22 @@ class linkedList {
 	}
 
 }
+/**
+ * Class for solution.
+ */
 public class Solution {
-
+	/**
+	 * Constructs the object.
+	 */
 	private Solution() {
 
 	}
-	public static void main(String[] args) {
+	/**
+	 * client program.
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
 		Scanner sc = new Scanner(System.in);
 		linkedList llobj = new linkedList();
 		while (sc.hasNext()) {
@@ -88,14 +170,14 @@ public class Solution {
 			switch (tokens[0]) {
 			case "insertAt":
 				if (Integer.parseInt(tokens[1]) < 0 || Integer.parseInt(
-                        tokens[1]) > llobj.length() + 1) {
-                        System.out.println("Can't insert at this position.");
-                    } else {
-                        llobj.insertAt(Integer.parseInt(tokens[1]),
-                            Integer.parseInt(tokens[2]));
-                        System.out.println(llobj.toString());
-                    }
-                    break;
+				            tokens[1]) > llobj.length() + 1) {
+					System.out.println("Can't insert at this position.");
+				} else {
+					llobj.insertAt(Integer.parseInt(tokens[1]),
+					               Integer.parseInt(tokens[2]));
+					System.out.println(llobj.toString());
+				}
+				break;
 			case "reverse":
 				if (!llobj.isEmpty()) {
 					llobj.reverse();
