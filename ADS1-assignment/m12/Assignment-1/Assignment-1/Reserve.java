23674f48edd971750571a46c4c7a76d6e29659e4
harshithaRao5 <harshithanallani@msitprogram.net>
2018-10-06 13:06:13 +0530

    "ads1_m12_a1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS1-assignment/m12/Assignment-1/Assignment-1/Reserve.java
+++ ADS1-assignment/m12/Assignment-1/Assignment-1/Reserve.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 public class Reserve {
 	private int vacancies;
 	private int unreserved;
@@ -11,31 +12,48 @@ public class Reserve {
 		this.sc = sc1;
 		this.st = st1;
 	}
+	private StudentDetails[] studentArray = new StudentDetails[10];
+	private int size = 0;
+	public int size() {
+		return size;
+	}
+	public void resize() {
+		studentArray = Arrays.copyOf(studentArray,
+		                             2 * studentArray.length);
+	}
+	public void add(final StudentDetails student) {
+		if (size == studentArray.length) {
+			resize();
+		}
+		studentArray[size++] = student;
+	}
 	public void reservation() {
 		Sort obj = new Sort();
+		for (int s = 0; s < size; s++) {
 			for (int i = 0; i <= unreserved; i++) {
-				if (StudentDetails.getCategory().equals("open")) {
+				if (studentArray[s].getCategory().equals("open")) {
 					obj.toString();
 					break;
 				}
 			}
 			for (int j = 0; j <= bc; j++) {
-				if (StudentDetails.getCategory().equals("BC")) {
+				if (studentArray[s].getCategory().equals("BC")) {
 					obj.toString();
 					break;
 				}
 			}
 			for (int k = 0; k <= sc; k++) {
-				if (StudentDetails.getCategory().equals("SC")) {
+				if (studentArray[s].getCategory().equals("SC")) {
 					obj.toString();
 					break;
 				}
 			}
 			for (int l = 0; l <= st; l++) {
-				if (StudentDetails.getCategory().equals("ST")) {
+				if (studentArray[s].getCategory().equals("ST")) {
 					obj.toString();
 					break;
 				}
 			}
 		}
 	}
+}
