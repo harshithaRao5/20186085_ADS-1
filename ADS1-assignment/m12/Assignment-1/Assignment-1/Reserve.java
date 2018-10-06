6cd79a5c09faa441d1c7edadb7d68a8d6ec4c48e
harshithaRao5 <harshithanallani@msitprogram.net>
2018-10-06 13:18:12 +0530

    "ads1_m12_a1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS1-assignment/m12/Assignment-1/Assignment-1/Reserve.java
+++ ADS1-assignment/m12/Assignment-1/Assignment-1/Reserve.java
@@ -1,36 +1,85 @@
 import java.util.Arrays;
+/**
+ * Class for reserve.
+ */
 public class Reserve {
+	/**
+	 * { var_description }
+	 */
 	private int vacancies;
+	/**
+	 * { var_description }
+	 */
 	private int unreserved;
+	/**
+	 * { var_description }
+	 */
 	private int bc;
+	/**
+	 * { var_description }
+	 */
 	private int sc;
+	/**
+	 * { var_description }
+	 */
 	private int st;
+	/**
+	 * { var_description }
+	 */
 	private StudentDetails[] studentArray = new StudentDetails[10];
+	/**
+	 * { var_description }
+	 */
 	private int size = 0;
-	Reserve(int vac, int ur, int bc1, int sc1, int st1) {
+	/**
+	 * Constructs the object.
+	 *
+	 * @param      vac   The vac
+	 * @param      ur    { parameter_description }
+	 * @param      bc1   The bc 1
+	 * @param      sc1   The screen 1
+	 * @param      st1   The st 1
+	 */
+	Reserve(final int vac, final int ur, final int bc1, final int sc1, final int st1) {
 		this.vacancies = vac;
 		this.unreserved = ur;
 		this.bc = bc1;
 		this.sc = sc1;
 		this.st = st1;
 	}
+	/**
+	 * { function_description }
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
 	public int size() {
 		return size;
 	}
+	/**
+	 * { function_description }
+	 */
 	public void resize() {
 		studentArray = Arrays.copyOf(studentArray,
 		                             2 * studentArray.length);
 	}
+	/**
+	 * { function_description }
+	 *
+	 * @param      student  The student
+	 */
 	public void add(final StudentDetails student) {
 		if (size == studentArray.length) {
 			resize();
 		}
 		studentArray[size++] = student;
 	}
+	/**
+	 * { function_description }
+	 */
 	public void reservation() {
 		Sort obj = new Sort();
 		for (int s = 0; s < studentArray.length; s++) {
-					System.out.println("hello");
+			//System.out.println("hello");
 			for (int i = 0; i <= unreserved; i++) {
 				if (studentArray[s].getCategory().equals("open")) {
 					obj.toString();
--- ADS1-assignment/m12/Assignment-1/Assignment-1/Solution.java
+++ ADS1-assignment/m12/Assignment-1/Assignment-1/Solution.java
@@ -1,15 +1,50 @@
 import java.util.Scanner;
 import java.util.Arrays;
+/**
+ * Class for student details.
+ */
 class StudentDetails implements Comparable<StudentDetails> {
+	/**
+	 * { var_description }
+	 */
 	private String name;
+	/**
+	 * { var_description }
+	 */
 	private String dateOfBirth;
+	/**
+	 * { var_description }
+	 */
 	private int subject1;
+	/**
+	 * { var_description }
+	 */
 	private int subject2;
+	/**
+	 * { var_description }
+	 */
 	private int subject3;
+	/**
+	 * { var_description }
+	 */
 	private int totalmarks;
+	/**
+	 * { var_description }
+	 */
 	private static String category;
-	StudentDetails(String n, String dob, int s1,
-	               int s2, int s3, int tm, String rc) {
+	/**
+	 * Constructs the object.
+	 *
+	 * @param      n     { parameter_description }
+	 * @param      dob   The dob
+	 * @param      s1    The s 1
+	 * @param      s2    The s 2
+	 * @param      s3    The s 3
+	 * @param      tm    The time
+	 * @param      rc    The rectangle
+	 */
+	StudentDetails(final String n, final String dob, final int s1,
+	               final int s2, final int s3, final int tm, final String rc) {
 		this.name = n;
 		this.dateOfBirth = dob;
 		this.subject1 = s1;
@@ -18,28 +53,70 @@ class StudentDetails implements Comparable<StudentDetails> {
 		this.totalmarks = tm;
 		this.category = rc;
 	}
+	/**
+	 * Gets the name.
+	 *
+	 * @return     The name.
+	 */
 	public String getName() {
 		return this.name;
 	}
+	/**
+	 * Gets the dob.
+	 *
+	 * @return     The dob.
+	 */
 	public String getDOB() {
 		return this.dateOfBirth;
 	}
+	/**
+	 * Gets the subject 1.
+	 *
+	 * @return     The subject 1.
+	 */
 	public int getSubject1() {
 		return this.subject1;
 	}
+	/**
+	 * Gets the subject 2.
+	 *
+	 * @return     The subject 2.
+	 */
 	public int getSubject2() {
 		return this.subject2;
 	}
+	/**
+	 * Gets the subject 3.
+	 *
+	 * @return     The subject 3.
+	 */
 	public int getSubject3() {
 		return this.subject3;
 	}
+	/**
+	 * Gets the totalmarks.
+	 *
+	 * @return     The totalmarks.
+	 */
 	public int getTotalmarks() {
 		return this.totalmarks;
 	}
+	/**
+	 * Gets the category.
+	 *
+	 * @return     The category.
+	 */
 	public static String getCategory() {
 		return category;
 	}
-	public int compareTo(StudentDetails that) {
+	/**
+	 * { function_description }
+	 *
+	 * @param      that  The that
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
+	public int compareTo(final StudentDetails that) {
 		//Date dateobj = new Date();
 		if (this.getTotalmarks() > that.getTotalmarks()) {
 			return 1;
@@ -64,26 +141,58 @@ class StudentDetails implements Comparable<StudentDetails> {
 		return 0;
 	}
 }
+/**
+ * Class for sort.
+ */
 class Sort {
+	/**
+	 * { var_description }
+	 */
 	private StudentDetails[] studentArray;
+	/**
+	 * { var_description }
+	 */
 	private int size;
+	/**
+	 * { var_description }
+	 */
+	private static int TEN = 10;
+	/**
+	 * Constructs the object.
+	 */
 	Sort() {
-		this.studentArray = new StudentDetails[10];
+		this.studentArray = new StudentDetails[TEN];
 		this.size = 0;
 	}
+	/**
+	 * { function_description }
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
 	public int size() {
 		return size;
 	}
+	/**
+	 * { function_description }
+	 */
 	public void resize() {
 		studentArray = Arrays.copyOf(studentArray,
 		                             2 * studentArray.length);
 	}
+	/**
+	 * { function_description }
+	 *
+	 * @param      student  The student
+	 */
 	public void add(final StudentDetails student) {
 		if (size == studentArray.length) {
 			resize();
 		}
 		studentArray[size++] = student;
 	}
+	/**
+	 * { function_description }
+	 */
 	public void insertionsort() {
 		for (int i = 0; i < size; i++) {
 			for (int j = i; j > 0; j--) {
@@ -95,27 +204,50 @@ class Sort {
 			}
 		}
 	}
-
+	/**
+	 * { function_description }
+	 *
+	 * @param      a     { parameter_description }
+	 * @param      i     { parameter_description }
+	 * @param      j     { parameter_description }
+	 */
 	public void exchange(final Comparable[] a, final int i, final int j) {
 		Comparable swap = a[i];
 		a[i] = a[j];
 		a[j] = swap;
 	}
-
+	/**
+	 * Returns a string representation of the object.
+	 *
+	 * @return     String representation of the object.
+	 */
 	public String toString() {
 		int k = 0;
 		String str = "";
 		for (k = 0; k < size - 1; k++) {
-			str += studentArray[k].getName() + "," + studentArray[k].getTotalmarks() + "," + studentArray[k].getCategory()+"\n";
+			str += studentArray[k].getName() + "," + studentArray[k].getTotalmarks() + "," + studentArray[k].getCategory() + "\n";
 		}
 		str += studentArray[k].getName() + "," + studentArray[k].getTotalmarks() + "," + studentArray[k].getCategory();
 		return str;
 	}
 
 }
-
+/**
+ * Class for solution.
+ */
 public class Solution {
-	public static void main(String[] args) {
+	/**
+	 * Constructs the object.
+	 */
+	private Solution() {
+
+	}
+	/**
+	 * { function_description }
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
 		Scanner sc = new Scanner(System.in);
 		Sort sortobj = new Sort();
 		int students = Integer.parseInt(sc.nextLine());
@@ -128,9 +260,9 @@ public class Solution {
 		while (sc.hasNext()) {
 			String[] tokens = sc.nextLine().split(",");
 			sortobj.add(new StudentDetails(tokens[0], tokens[1],
-			Integer.parseInt(tokens[2]), Integer.parseInt(tokens[3]),
-			Integer.parseInt(tokens[4]),
-			Integer.parseInt(tokens[5]), tokens[6]));
+			                               Integer.parseInt(tokens[2]), Integer.parseInt(tokens[3]),
+			                               Integer.parseInt(tokens[4]),
+			                               Integer.parseInt(tokens[5]), tokens[6]));
 		}
 		sortobj.insertionsort();
 		System.out.println(sortobj);
