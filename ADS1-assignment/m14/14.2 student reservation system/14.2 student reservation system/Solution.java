f30fc7ab12b02160ef7ac6a694ad8b1683b6da82
harshithaRao5 <harshithanallani@msitprogram.net>
2018-10-09 18:56:26 +0530

    "ads1_m14_a2: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS1-assignment/m14/14.2 student reservation system/14.2 student reservation system/Solution.java
+++ ADS1-assignment/m14/14.2 student reservation system/14.2 student reservation system/Solution.java
@@ -399,17 +399,17 @@ class Insertion {
      */
     public static void sort(Comparable[] a) {
         int n = a.length;
-        for (int k = n / 2; k >= 1; k--)
+        for (int k = n / 2; k >= 0; k--)
             sink(a, k, n);
-        while (n > 1) {
-            exch(a, 1, n);
-            sink(a, 1, --n);
+        while (n > 0) {
+            exch(a, 0, n);
+            sink(a, 0, --n);
         }
     }
     private static void sink(Comparable[] pq, int k, int n) {
         while (2 * k <= n) {
             int j = 2 * k;
-            if (j < n && less(pq, j, j + 1)) j++;
+            if (j < n && less(pq, j+1, j + 2)) j++;
             if (!less(pq, k, j)) break;
             exch(pq, k, j);
             k = j;
