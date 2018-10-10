e0ecea9fdd0782557e370d66d95c73bf9f68072b
harshithaRao5 <harshithanallani@msitprogram.net>
2018-10-10 15:30:00 +0530

    "ads1_m15_a1: 0/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS1-assignment/m15/15.1 BinarySearchSymbolTable/15.1 BinarySearchSymbolTable/BinarySearchST.java
+++ ADS1-assignment/m15/15.1 BinarySearchSymbolTable/15.1 BinarySearchSymbolTable/BinarySearchST.java
@@ -6,23 +6,15 @@ public class BinarySearchST<Key extends Comparable<Key>, Value> {
     private Value[] vals;
     private int n = 0;
 
-    /**
-     * Initializes an empty symbol table.
-     */
     public BinarySearchST() {
         this(INIT_CAPACITY);
     }
 
-    /**
-     * Initializes an empty symbol table with the specified initial capacity.
-     * @param capacity the maximum capacity
-     */
     public BinarySearchST(int capacity) {
         keys = (Key[]) new Comparable[capacity];
         vals = (Value[]) new Object[capacity];
     }
 
-    // resize the underlying arrays
     private void resize(int capacity) {
         assert capacity >= n;
         Key[]   tempk = (Key[])   new Comparable[capacity];
@@ -35,47 +27,20 @@ public class BinarySearchST<Key extends Comparable<Key>, Value> {
         keys = tempk;
     }
 
-    /**
-     * Returns the number of key-value pairs in this symbol table.
-     *
-     * @return the number of key-value pairs in this symbol table
-     */
     public int size() {
         return n;
     }
 
-    /**
-     * Returns true if this symbol table is empty.
-     *
-     * @return {@code true} if this symbol table is empty;
-     *         {@code false} otherwise
-     */
+
     public boolean isEmpty() {
         return size() == 0;
     }
 
-
-    /**
-     * Does this symbol table contain the given key?
-     *
-     * @param  key the key
-     * @return {@code true} if this symbol table contains {@code key} and
-     *         {@code false} otherwise
-     * @throws IllegalArgumentException if {@code key} is {@code null}
-     */
     public boolean contains(Key key) {
         if (key == null) throw new IllegalArgumentException("argument to contains() is null");
         return get(key) != null;
     }
 
-    /**
-     * Returns the value associated with the given key in this symbol table.
-     *
-     * @param  key the key
-     * @return the value associated with the given key if the key is in the symbol table
-     *         and {@code null} if the key is not in the symbol table
-     * @throws IllegalArgumentException if {@code key} is {@code null}
-     */
     public Value get(Key key) {
         if (key == null) throw new IllegalArgumentException("argument to get() is null");
         if (isEmpty()) return null;
@@ -84,13 +49,6 @@ public class BinarySearchST<Key extends Comparable<Key>, Value> {
         return null;
     }
 
-    /**
-     * Returns the number of keys in this symbol table strictly less than {@code key}.
-     *
-     * @param  key the key
-     * @return the number of keys in the symbol table strictly less than {@code key}
-     * @throws IllegalArgumentException if {@code key} is {@code null}
-     */
     public int rank(Key key) {
         if (key == null) throw new IllegalArgumentException("argument to rank() is null");
 
@@ -105,35 +63,17 @@ public class BinarySearchST<Key extends Comparable<Key>, Value> {
         return lo;
     }
 
-
-
-    /**
-     * Inserts the specified key-value pair into the symbol table, overwriting the old
-     * value with the new value if the symbol table already contains the specified key.
-     * Deletes the specified key (and its associated value) from this symbol table
-     * if the specified value is {@code null}.
-     *
-     * @param  key the key
-     * @param  val the value
-     * @throws IllegalArgumentException if {@code key} is {@code null}
-     */
     public void put(Key key, Value val)  {
         if (key == null) throw new IllegalArgumentException("first argument to put() is null");
-
         if (val == null) {
             delete(key);
             return;
         }
-
         int i = rank(key);
-
-        // key is already in table
         if (i < n && keys[i].compareTo(key) == 0) {
             vals[i] = val;
             return;
         }
-
-        // insert new key-value pair
         if (n == keys.length) resize(2*keys.length);
 
         for (int j = n; j > i; j--)  {
@@ -147,110 +87,50 @@ public class BinarySearchST<Key extends Comparable<Key>, Value> {
         assert check();
     }
 
-    /**
-     * Removes the specified key and associated value from this symbol table
-     * (if the key is in the symbol table).
-     *
-     * @param  key the key
-     * @throws IllegalArgumentException if {@code key} is {@code null}
-     */
     public void delete(Key key) {
         if (key == null) throw new IllegalArgumentException("argument to delete() is null");
         if (isEmpty()) return;
-
-        // compute rank
         int i = rank(key);
-
-        // key not in table
         if (i == n || keys[i].compareTo(key) != 0) {
             return;
         }
-
         for (int j = i; j < n-1; j++)  {
             keys[j] = keys[j+1];
             vals[j] = vals[j+1];
         }
-
         n--;
-        keys[n] = null;  // to avoid loitering
+        keys[n] = null;
         vals[n] = null;
-
-        // resize if 1/4 full
         if (n > 0 && n == keys.length/4) resize(keys.length/2);
 
         assert check();
     }
 
-    /**
-     * Removes the smallest key and associated value from this symbol table.
-     *
-     * @throws NoSuchElementException if the symbol table is empty
-     */
     public void deleteMin() {
         if (isEmpty()) throw new NoSuchElementException("Symbol table underflow error");
         delete(min());
     }
 
-    /**
-     * Removes the largest key and associated value from this symbol table.
-     *
-     * @throws NoSuchElementException if the symbol table is empty
-     */
     public void deleteMax() {
         if (isEmpty()) throw new NoSuchElementException("Symbol table underflow error");
         delete(max());
     }
 
-
-   /***************************************************************************
-    *  Ordered symbol table methods.
-    ***************************************************************************/
-
-   /**
-     * Returns the smallest key in this symbol table.
-     *
-     * @return the smallest key in this symbol table
-     * @throws NoSuchElementException if this symbol table is empty
-     */
     public Key min() {
         if (isEmpty()) throw new NoSuchElementException("called min() with empty symbol table");
         return keys[0];
     }
 
-    /**
-     * Returns the largest key in this symbol table.
-     *
-     * @return the largest key in this symbol table
-     * @throws NoSuchElementException if this symbol table is empty
-     */
     public Key max() {
         if (isEmpty()) throw new NoSuchElementException("called max() with empty symbol table");
         return keys[n-1];
     }
-
-    /**
-     * Return the kth smallest key in this symbol table.
-     *
-     * @param  k the order statistic
-     * @return the {@code k}th smallest key in this symbol table
-     * @throws IllegalArgumentException unless {@code k} is between 0 and
-     *        <em>n</em>–1
-     */
     public Key select(int k) {
         if (k < 0 || k >= size()) {
             throw new IllegalArgumentException("called select() with invalid argument: " + k);
         }
         return keys[k];
     }
-
-    /**
-     * Returns the largest key in this symbol table less than or equal to {@code key}.
-     *
-     * @param  key the key
-     * @return the largest key in this symbol table less than or equal to {@code key}
-     * @throws NoSuchElementException if there is no such key
-     * @throws IllegalArgumentException if {@code key} is {@code null}
-     */
     public Key floor(Key key) {
         if (key == null) throw new IllegalArgumentException("argument to floor() is null");
         int i = rank(key);
@@ -258,15 +138,6 @@ public class BinarySearchST<Key extends Comparable<Key>, Value> {
         if (i == 0) return null;
         else return keys[i-1];
     }
-
-    /**
-     * Returns the smallest key in this symbol table greater than or equal to {@code key}.
-     *
-     * @param  key the key
-     * @return the smallest key in this symbol table greater than or equal to {@code key}
-     * @throws NoSuchElementException if there is no such key
-     * @throws IllegalArgumentException if {@code key} is {@code null}
-     */
     public Key ceiling(Key key) {
         if (key == null) throw new IllegalArgumentException("argument to ceiling() is null");
         int i = rank(key);
@@ -274,16 +145,6 @@ public class BinarySearchST<Key extends Comparable<Key>, Value> {
         else return keys[i];
     }
 
-    /**
-     * Returns the number of keys in this symbol table in the specified range.
-     *
-     * @param lo minimum endpoint
-     * @param hi maximum endpoint
-     * @return the number of keys in this symbol table between {@code lo}
-     *         (inclusive) and {@code hi} (inclusive)
-     * @throws IllegalArgumentException if either {@code lo} or {@code hi}
-     *         is {@code null}
-     */
     public int size(Key lo, Key hi) {
         if (lo == null) throw new IllegalArgumentException("first argument to size() is null");
         if (hi == null) throw new IllegalArgumentException("second argument to size() is null");
@@ -318,7 +179,6 @@ public class BinarySearchST<Key extends Comparable<Key>, Value> {
     // public Iterable<Key> keys(Key lo, Key hi) {
     //     if (lo == null) throw new IllegalArgumentException("first argument to keys() is null");
     //     if (hi == null) throw new IllegalArgumentException("second argument to keys() is null");
-
     //     Queue<Key> queue = new Queue<Key>();
     //     if (lo.compareTo(hi) > 0) return queue;
     //     for (int i = rank(lo); i < rank(hi); i++)
@@ -327,23 +187,16 @@ public class BinarySearchST<Key extends Comparable<Key>, Value> {
     //     return queue;
     // }
 
-
-   /***************************************************************************
-    *  Check internal invariants.
-    ***************************************************************************/
-
     private boolean check() {
         return isSorted() && rankCheck();
     }
 
-    // are the items in the array in ascending order?
     private boolean isSorted() {
         for (int i = 1; i < size(); i++)
             if (keys[i].compareTo(keys[i-1]) < 0) return false;
         return true;
     }
 
-    // check that rank(select(i)) = i
     private boolean rankCheck() {
         for (int i = 0; i < size(); i++)
             if (i != rank(select(i))) return false;
--- ADS1-assignment/m15/15.1 BinarySearchSymbolTable/15.1 BinarySearchSymbolTable/Solution.java
+++ ADS1-assignment/m15/15.1 BinarySearchSymbolTable/15.1 BinarySearchSymbolTable/Solution.java
@@ -1,4 +1,29 @@
 import java.util.Scanner;
+// class BinarySearchST<Key extends Comparable<Key>, Value> {
+// 	private static final int INIT_CAPACITY = 2;
+// 	private Key[] keys;
+// 	private Value[] vals;
+// 	private int n = 0;
+// 	BinarySearchST() {
+// 		this(INIT_CAPACITY);
+// 	}
+
+// 	public BinarySearchST(int capacity) {
+// 		keys = (Key[]) new Comparable[capacity];
+// 		vals = (Value[]) new Object[capacity];
+// 	}
+// 	private void resize(int capacity) {
+// 		Key[]   tempk = (Key[])   new Comparable[capacity];
+//         Value[] tempv = (Value[]) new Object[capacity];
+//         for (int i = 0; i < n; i++) {
+//             tempk[i] = keys[i];
+//             tempv[i] = vals[i];
+//         }
+//         vals = tempv;
+//         keys = tempk;
+// 	}
+
+// }
 public final class Solution {
 	private Solution() {
 
@@ -34,7 +59,8 @@ public final class Solution {
 				case "get":
 				System.out.println(bst.get(tokens[1]));
 				break;
-
+				default:
+				break;
 
 				}
 			}
