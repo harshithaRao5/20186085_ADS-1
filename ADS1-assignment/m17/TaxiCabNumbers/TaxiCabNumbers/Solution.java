92979537a4ec71a63cd420b345c12f93f1955d58
harshithaRao5 <harshithanallani@msitprogram.net>
2018-10-12 17:12:35 +0530

    "taxi_cab_numbers: 0/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS1-assignment/m17/TaxiCabNumbers/TaxiCabNumbers/Solution.java
+++ ADS1-assignment/m17/TaxiCabNumbers/TaxiCabNumbers/Solution.java
@@ -1,27 +1,85 @@
 import java.util.Scanner;
+/**
+ * Class for cube sum.
+ */
 class CubeSum implements Comparable<CubeSum> {
-    public final int sum;
-    public final int i;
-    public final int j;
-
+    /**
+     * variable declaration.
+     */
+    private final int sum;
+    /**
+     * variable declaration.
+     */
+    private final int i;
+    /**
+     * variable declaration.
+     */
+    private final int j;
+    /**
+     * Constructs the object.
+     *
+     * @param      i integer.
+     * @param      j integer.
+     */
     public CubeSum(int i, int j) {
         this.sum = i * i * i + j * j * j;
         this.i = i;
         this.j = j;
     }
-
+    /**
+     * Gets the sum.
+     *
+     * @return     The sum.
+     */
+    public int getSum() {
+        return sum;
+    }
+    /**
+     * gets the number.
+     *
+     * @return number.
+     */
+    public int geti() {
+        return i;
+    }
+    /**
+     * gets the number.
+     *
+     * @return number.
+     */
+    public int getj() {
+        return j;
+    }
+    /**
+     * compare to method.
+     *
+     * @param      that  The that
+     *
+     * @return  integer.
+     */
     public int compareTo(CubeSum that) {
         if (this.sum < that.sum) return -1;
         if (this.sum > that.sum) return +1;
         return 0;
     }
-
+    /**
+     * Returns a string representation of the object.
+     *
+     * @return     String representation of the object.
+     */
     public String toString() {
         return sum + " = " + i + "^3" + " + " + j + "^3";
     }
 }
-
+/**
+ * client program.
+ */
 public final class Solution {
+    /**
+     * main method.
+     *
+     * @param      args  The arguments
+     */
     public static void main(String[] args) {
         int n = 600;
         Scanner scan = new Scanner(System.in);
@@ -37,7 +95,8 @@ public final class Solution {
         // find smallest sum, print it out, and update
         while (!pq.isEmpty()) {
             CubeSum s = pq.delMin();
-            if (temp == s.sum) {
+            System.out.println(s.getSum()+" "+temp);
+            if (temp == s.getSum()) {
                 count++;
             } else {
                 count = 0;
@@ -45,13 +104,13 @@ public final class Solution {
             if(count == M - 1) {
                 N--;
                 if (N == 0) {
-                    System.out.println(s.sum);
+                    System.out.println(s.getSum());
                     break;
                 }
             }
-            temp = s.sum;
-            if (s.j < n) {
-                pq.insert(new CubeSum(s.i, s.j + 1));
+            temp = s.getSum();
+            if (s.getj()< n) {
+                pq.insert(new CubeSum(s.geti(), s.getj()+ 1));
             }
             }
         }
