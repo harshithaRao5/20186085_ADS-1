327e895c1505adb8e126838853f848a63f5b5bd4
harshithaRao5 <harshithanallani@msitprogram.net>
2018-10-25 17:41:34 +0530

    "ads1_m22_a1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS1-assignment/m21/22.1 Ransom Note/22.1 Ransom Note/Solution.java
+++ ADS1-assignment/m21/22.1 Ransom Note/22.1 Ransom Note/Solution.java
@@ -1,30 +1,41 @@
 import java.util.Scanner;
+/**
+ * client program.
+ */
 public final class Solution {
+	/**
+	 * Constructs the object.
+	 */
 	private Solution() {
-
+		//unused constructor.
 	}
+	/**
+	 * main method.
+	 *
+	 * @param      args  The arguments
+	 */
 	public static void main(final String[] args) {
 		Scanner sc = new Scanner(System.in);
 		SeparateChainingHashST<String, Integer> hashobj = new SeparateChainingHashST<String, Integer>();
 		String[] length = sc.nextLine().split(" ");
 		String[] s1 = sc.nextLine().split(" ");
-		for(int i = 0; i < s1.length;i++) {
-			if(hashobj.contains(s1[i])) {
-				hashobj.put(s1[i], hashobj.get(s1[i])+1);
-			}else{
-				hashobj.put(s1[i],1);
+		for (int i = 0; i < s1.length; i++) {
+			if (hashobj.contains(s1[i])) {
+				hashobj.put(s1[i], hashobj.get(s1[i]) + 1);
+			} else {
+				hashobj.put(s1[i], 1);
 			}
 		}
 		String[] s2 = sc.nextLine().split(" ");
-		for(int i = 0;i<s2.length;i++) {
-			if(hashobj.contains(s2[i])) {
-				if(hashobj.get(s2[i])==0){
+		for (int i = 0; i < s2.length; i++) {
+			if (hashobj.contains(s2[i])) {
+				if (hashobj.get(s2[i]) == 0) {
 					System.out.println("No");
 					return;
-				}else{
-					hashobj.put(s2[i],hashobj.get(s2[i])-1);
+				} else {
+					hashobj.put(s2[i], hashobj.get(s2[i]) - 1);
 				}
-			}else{
+			} else {
 				System.out.println("No");
 				return;
 			}
