37c52b1d7ffcb4b7ebc5bf28c52a1312965372b4
harshithaRao5 <harshithanallani@msitprogram.net>
2018-10-26 17:10:59 +0530

    "ads1_m23_a1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS1-assignment/m23/23.1 Hashing with Linear Probing/23.1 Hashing with Linear Probing/Solution.java
+++ ADS1-assignment/m23/23.1 Hashing with Linear Probing/23.1 Hashing with Linear Probing/Solution.java
@@ -1,40 +1,42 @@
 import java.util.Scanner;
+/**
+ * client program.
+ */
 public final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
-		//unused constructor.
-	}
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		LinearProbingHashST<String, Integer> hashobj
-		    = new LinearProbingHashST<String, Integer>();
-		int n = Integer.parseInt(sc.nextLine());
-		while (n > 0) {
-			String[] tokens = sc.nextLine().split(" ");
-			switch (tokens[0]) {
-			case "put" :
-				hashobj.put(tokens[1], Integer.parseInt(tokens[2]));
-				break;
-			case "get" :
-				System.out.println(hashobj.get(tokens[1]));
-				break;
-			case "display" :
-				try {
-					System.out.println(hashobj.display());
-				}
-				catch(Exception ex) {
-					System.out.println("{}");
-				}
-				break;
-			case "delete" :
-				hashobj.delete(tokens[1]);
-				break;
-			default:
-				break;
-			}
-			n--;
-		}
-	}
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //unused constructor.
+    }
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        LinearProbingHashST<String, Integer> hashobj
+            = new LinearProbingHashST<String, Integer>();
+        int n = Integer.parseInt(sc.nextLine());
+        while (n > 0) {
+            String[] tokens = sc.nextLine().split(" ");
+            switch (tokens[0]) {
+            case "put" :
+                hashobj.put(tokens[1], Integer.parseInt(tokens[2]));
+                break;
+            case "get" :
+                System.out.println(hashobj.get(tokens[1]));
+                break;
+            case "display" :
+                try {
+                    System.out.println(hashobj.display());
+                } catch (Exception ex) {
+                    System.out.println("{}");
+                }
+                break;
+            case "delete" :
+                hashobj.delete(tokens[1]);
+                break;
+            default:
+                break;
+            }
+            n--;
+        }
+    }
 }
