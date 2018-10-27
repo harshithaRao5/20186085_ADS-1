14a85d0544047c8e36ad84d6fdcbbe53875e746a
harshithaRao5 <harshithanallani@msitprogram.net>
2018-10-27 13:15:49 +0530

    "m24_a2_1: 1/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS1-assignment/m24/2/2/Solution.java
+++ ADS1-assignment/m24/2/2/Solution.java
@@ -4,17 +4,17 @@ import java.util.Scanner;
  */
 class StudentDetails {
 	/**
-	 * { var_description }
+	 * variable declaration.
 	 */
 	private int roll_number;
 	/**
-	 * { var_description }
+	 * variable declaration.
 	 */
 	String student_name;
 	/**
 	 * Constructs the object.
 	 *
-	 * @param      rn    { parameter_description }
+	 * @param      rn    integer
 	 * @param      sn    The serial number
 	 */
 	StudentDetails(final int rn, final String sn) {
@@ -40,7 +40,7 @@ class StudentDetails {
 
 }
 /**
- * { item_description }
+ * client class.
  */
 public final class Solution {
 	/**
@@ -50,40 +50,47 @@ public final class Solution {
 
 	}
 	/**
-	 * { function_description }
+	 * main method.
 	 *
 	 * @param      args  The arguments
 	 */
 	public static void main(final String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = Integer.parseInt(sc.nextLine());
-		BST<Double, StudentDetails> bstobj = new BST<Double, StudentDetails>();
+		BST<Double, StudentDetails> bstobj =
+		new BST<Double, StudentDetails>();
 		for (int i = 0; i < n; i++) {
 			String[] tokens = sc.nextLine().split(",");
 			bstobj.put(Double.parseDouble(tokens[2]),
-			           new StudentDetails(Integer.parseInt(tokens[0]), tokens[1]));
+			    new StudentDetails(Integer.parseInt(tokens[0]), tokens[1]));
 		}
 		int m = Integer.parseInt(sc.nextLine());
 		for (int j = 0; j < m; j++) {
 			String[] input = sc.nextLine().split(" ");
 			if (input[0].equals("BE")) {
-				String[] s = bstobj.keys(Double.parseDouble(input[1]), Double.parseDouble(input[2])).toString().split(" ");
+				String[] s = bstobj.keys(Double.parseDouble(input[1]),
+					Double.parseDouble(input[2])).toString().split(" ");
 				for (int i = 0; i < s.length; i++) {
-					System.out.println(bstobj.get(Double.parseDouble(s[i])).student_name);
+					System.out.println(bstobj.get(
+						Double.parseDouble(s[i])).student_name);
 				}
 			} else if (input[0].equals("LE")) {
 				double min = bstobj.min();
-				String[] s = bstobj.keys(min, Double.parseDouble(input[1])).toString().split(" ");
+				String[] s = bstobj.keys(min, Double.parseDouble
+					(input[1])).toString().split(" ");
 				for (int i = 0; i < s.length; i++) {
-					System.out.println(bstobj.get(Double.parseDouble(s[i])).student_name);
+					System.out.println(bstobj.get(
+						Double.parseDouble(s[i])).student_name);
 				}
 
 
 			} else if (input[0].equals("GE")) {
 				double max = bstobj.max();
-				String[] s = bstobj.keys(max, Double.parseDouble(input[1])).toString().split(" ");
+				String[] s = bstobj.keys(max, Double.parseDouble
+					(input[1])).toString().split(" ");
 				for (int i = 0; i < s.length; i++) {
-					System.out.println(bstobj.get(Double.parseDouble(s[i])).student_name);
+					System.out.println(bstobj.get
+						(Double.parseDouble(s[i])).student_name);
 				}
 
 			}
