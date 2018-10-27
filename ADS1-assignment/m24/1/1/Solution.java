f939b55d3938b560469e092e5ff86a82a3ffd440
harshithaRao5 <harshithanallani@msitprogram.net>
2018-10-27 11:35:18 +0530

    "m24_a1_1: 4/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS1-assignment/m24/1/1/Solution.java
+++ ADS1-assignment/m24/1/1/Solution.java
@@ -3,41 +3,41 @@ import java.util.Scanner;
  * client program.
  */
 public final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
-		//unused constructor.
-	}
-	/**
-	 * main method.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = Integer.parseInt(sc.nextLine());
-		LinearProbingHashST<Integer, String> hashobj =
-		    new LinearProbingHashST<Integer, String>();
-		for (int i = 0; i < n; i++) {
-			String[] tokens = sc.nextLine().split(",");
-			String s = tokens[1] + "," + tokens[2];
-			hashobj.put(Integer.parseInt(tokens[0]), s);
-		}
-		int m = Integer.parseInt(sc.nextLine());
-		for (int j = 0; j < m; j++) {
-			String[] tokens = sc.nextLine().split(" ");
-			String s = hashobj.get(Integer.parseInt(tokens[1]));
-			if (s == null) {
-				System.out.println("Student doesn't exists...");
-			} else {
-				String[] tokens1 = s.split(",");
-				if (Integer.parseInt(tokens[2]) == 1) {
-					System.out.println(tokens1[0]);
-				} else {
-					System.out.println(Double.parseDouble(tokens1[1]));
-				}
-			}
-		}
-	}
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //unused constructor.
+    }
+    /**
+     * main method.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        LinearProbingHashST<Integer, String> hashobj =
+            new LinearProbingHashST<Integer, String>();
+        for (int i = 0; i < n; i++) {
+            String[] tokens = sc.nextLine().split(",");
+            String s = tokens[1] + "," + tokens[2];
+            hashobj.put(Integer.parseInt(tokens[0]), s);
+        }
+        int m = Integer.parseInt(sc.nextLine());
+        for (int j = 0; j < m; j++) {
+            String[] tokens = sc.nextLine().split(" ");
+            String s = hashobj.get(Integer.parseInt(tokens[1]));
+            if (s == null) {
+                System.out.println("Student doesn't exists...");
+            } else {
+                String[] tokens1 = s.split(",");
+                if (Integer.parseInt(tokens[2]) == 1) {
+                    System.out.println(tokens1[0]);
+                } else {
+                    System.out.println(Double.parseDouble(tokens1[1]));
+                }
+            }
+        }
+    }
 }
