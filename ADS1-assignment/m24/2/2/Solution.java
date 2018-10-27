b5aa98b63cb0220ad750b630da8e2b04b4c856e4
harshithaRao5 <harshithanallani@msitprogram.net>
2018-10-27 13:11:14 +0530

    "m24_a2_1: 1/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS1-assignment/m24/2/2/Solution.java
+++ ADS1-assignment/m24/2/2/Solution.java
@@ -1,23 +1,59 @@
 import java.util.Scanner;
+/**
+ * Class for student details.
+ */
 class StudentDetails {
+	/**
+	 * { var_description }
+	 */
 	private int roll_number;
+	/**
+	 * { var_description }
+	 */
 	String student_name;
+	/**
+	 * Constructs the object.
+	 *
+	 * @param      rn    { parameter_description }
+	 * @param      sn    The serial number
+	 */
 	StudentDetails(final int rn, final String sn) {
 		this.roll_number = rn;
 		this.student_name = sn;
 	}
+	/**
+	 * Gets the roll number.
+	 *
+	 * @return     The roll number.
+	 */
 	public int getRollNumber() {
 		return this.roll_number;
 	}
+	/**
+	 * Gets the student name.
+	 *
+	 * @return     The student name.
+	 */
 	public String getStudentName() {
 		return student_name;
 	}
 
 }
+/**
+ * { item_description }
+ */
 public final class Solution {
+	/**
+	 * Constructs the object.
+	 */
 	private Solution() {
 
 	}
+	/**
+	 * { function_description }
+	 *
+	 * @param      args  The arguments
+	 */
 	public static void main(final String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = Integer.parseInt(sc.nextLine());
@@ -35,9 +71,20 @@ public final class Solution {
 				for (int i = 0; i < s.length; i++) {
 					System.out.println(bstobj.get(Double.parseDouble(s[i])).student_name);
 				}
-			} else if(input[0].equals("LE")) {
+			} else if (input[0].equals("LE")) {
+				double min = bstobj.min();
+				String[] s = bstobj.keys(min, Double.parseDouble(input[1])).toString().split(" ");
+				for (int i = 0; i < s.length; i++) {
+					System.out.println(bstobj.get(Double.parseDouble(s[i])).student_name);
+				}
 
-			} else if(input[0].equals("GE")) {
+
+			} else if (input[0].equals("GE")) {
+				double max = bstobj.max();
+				String[] s = bstobj.keys(max, Double.parseDouble(input[1])).toString().split(" ");
+				for (int i = 0; i < s.length; i++) {
+					System.out.println(bstobj.get(Double.parseDouble(s[i])).student_name);
+				}
 
 			}
 
