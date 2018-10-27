5db8e04690dcc0b32b0339e9b512467d28d9543a
harshithaRao5 <harshithanallani@msitprogram.net>
2018-10-27 12:27:21 +0530

    "m24_a2_1: 0/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS1-assignment/m24/2/2/Solution.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+class StudentDetails {
+	private int roll_number;
+	private String student_name;
+	StudentDetails(final int rn, final String sn) {
+		this.roll_number = rn;
+		this.student_name = sn;
+	}
+	public int getRollNumber() {
+		return this.roll_number;
+	}
+	public String getStudentName() {
+		return this.student_name;
+	}
+
+}
+public final class Solution {
+	private Solution() {
+
+	}
+	public static void main(final String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = Integer.parseInt(sc.nextLine());
+		BST<Double, StudentDetails> bstobj = new BST<Double, StudentDetails>();
+		for (int i = 0; i < n; i++) {
+			String[] tokens = sc.nextLine().split(",");
+			bstobj.put(Double.parseDouble(tokens[2]),
+			           new StudentDetails(Integer.parseInt(tokens[0]), tokens[1]));
+		}
+		int m = Integer.parseInt(sc.nextLine());
+		for (int j = 0; j < m; j++) {
+			String[] input = sc.nextLine().split(" ");
+			if(input[0].equals("BE")) {
+				System.out.println(bstobj.keys(Double.parseDouble(input[1]), Double.parseDouble(input[2])));
+			}
+
+		}
+	}
+}
