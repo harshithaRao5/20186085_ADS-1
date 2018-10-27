777d5389420233eca1d56c8845ea8b8bdc81a773
harshithaRao5 <harshithanallani@msitprogram.net>
2018-10-27 18:44:03 +0530

    "m24_a2_1: 1/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS1-assignment/m24/2/2/Solution.java
+++ ADS1-assignment/m24/2/2/Solution.java
@@ -57,8 +57,8 @@ public final class Solution {
     public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = Integer.parseInt(sc.nextLine());
-        BST<Double, StudentDetails> bstobj =
-        new BST<Double, StudentDetails>();
+        RedBlackBST<Double, StudentDetails> bstobj =
+        new RedBlackBST<Double, StudentDetails>();
         for (int i = 0; i < n; i++) {
             String[] tokens = sc.nextLine().split(",");
             bstobj.put(Double.parseDouble(tokens[2]),
@@ -76,16 +76,16 @@ public final class Solution {
                 }
             } else if (input[0].equals("LE")) {
                 double min = bstobj.min();
-                String[] s = bstobj.keys(min, Double.parseDouble(
-                    input[1])).toString().split(" ");
+                String[] s = bstobj.keys(min, Double.parseDouble
+                    (input[1])).toString().split(" ");
                 for (int i = 0; i < s.length; i++) {
                     System.out.println(bstobj.get(
                         Double.parseDouble(s[i])).studentname);
                 }
             } else if (input[0].equals("GE")) {
                 double max = bstobj.max();
-                String[] s = bstobj.keys(max, Double.parseDouble(
-                    input[1])).toString().split(" ");
+                String[] s = bstobj.keys(max, Double.parseDouble
+                    (input[1])).toString().split(" ");
                 for (int i = 0; i < s.length; i++) {
                     System.out.println(bstobj.get(
                         Double.parseDouble(s[i])).studentname);
