070b3f552c022e499b45496c0880088f204375a9
harshithaRao5 <harshithanallani@msitprogram.net>
2018-10-27 13:18:24 +0530

    "m24_a2_1: 1/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS1-assignment/m24/2/2/Solution.java
+++ ADS1-assignment/m24/2/2/Solution.java
@@ -3,98 +3,96 @@ import java.util.Scanner;
  * Class for student details.
  */
 class StudentDetails {
-	/**
-	 * variable declaration.
-	 */
-	private int roll_number;
-	/**
-	 * variable declaration.
-	 */
-	String student_name;
-	/**
-	 * Constructs the object.
-	 *
-	 * @param      rn    integer
-	 * @param      sn    The serial number
-	 */
-	StudentDetails(final int rn, final String sn) {
-		this.roll_number = rn;
-		this.student_name = sn;
-	}
-	/**
-	 * Gets the roll number.
-	 *
-	 * @return     The roll number.
-	 */
-	public int getRollNumber() {
-		return this.roll_number;
-	}
-	/**
-	 * Gets the student name.
-	 *
-	 * @return     The student name.
-	 */
-	public String getStudentName() {
-		return student_name;
-	}
+    /**
+     * variable declaration.
+     */
+    private int rollnumber;
+    /**
+     * variable declaration.
+     */
+    String studentname;
+    /**
+     * Constructs the object.
+     *
+     * @param      rn    integer
+     * @param      sn    The serial number
+     */
+    StudentDetails(final int rn, final String sn) {
+        this.rollnumber = rn;
+        this.studentname = sn;
+    }
+    /**
+     * Gets the roll number.
+     *
+     * @return     The roll number.
+     */
+    public int getRollNumber() {
+        return this.rollnumber;
+    }
+    /**
+     * Gets the student name.
+     *
+     * @return     The student name.
+     */
+    public String getStudentName() {
+        return studentname;
+    }
 
 }
 /**
  * client class.
  */
 public final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
 
-	}
-	/**
-	 * main method.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = Integer.parseInt(sc.nextLine());
-		BST<Double, StudentDetails> bstobj =
-		new BST<Double, StudentDetails>();
-		for (int i = 0; i < n; i++) {
-			String[] tokens = sc.nextLine().split(",");
-			bstobj.put(Double.parseDouble(tokens[2]),
-			    new StudentDetails(Integer.parseInt(tokens[0]), tokens[1]));
-		}
-		int m = Integer.parseInt(sc.nextLine());
-		for (int j = 0; j < m; j++) {
-			String[] input = sc.nextLine().split(" ");
-			if (input[0].equals("BE")) {
-				String[] s = bstobj.keys(Double.parseDouble(input[1]),
-					Double.parseDouble(input[2])).toString().split(" ");
-				for (int i = 0; i < s.length; i++) {
-					System.out.println(bstobj.get(
-						Double.parseDouble(s[i])).student_name);
-				}
-			} else if (input[0].equals("LE")) {
-				double min = bstobj.min();
-				String[] s = bstobj.keys(min, Double.parseDouble
-					(input[1])).toString().split(" ");
-				for (int i = 0; i < s.length; i++) {
-					System.out.println(bstobj.get(
-						Double.parseDouble(s[i])).student_name);
-				}
+    }
+    /**
+     * main method.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        BST<Double, StudentDetails> bstobj =
+        new BST<Double, StudentDetails>();
+        for (int i = 0; i < n; i++) {
+            String[] tokens = sc.nextLine().split(",");
+            bstobj.put(Double.parseDouble(tokens[2]),
+                new StudentDetails(Integer.parseInt(tokens[0]), tokens[1]));
+        }
+        int m = Integer.parseInt(sc.nextLine());
+        for (int j = 0; j < m; j++) {
+            String[] input = sc.nextLine().split(" ");
+            if (input[0].equals("BE")) {
+                String[] s = bstobj.keys(Double.parseDouble(input[1]),
+                    Double.parseDouble(input[2])).toString().split(" ");
+                for (int i = 0; i < s.length; i++) {
+                    System.out.println(bstobj.get(
+                        Double.parseDouble(s[i])).studentname);
+                }
+            } else if (input[0].equals("LE")) {
+                double min = bstobj.min();
+                String[] s = bstobj.keys(min, Double.parseDouble
+                    (input[1])).toString().split(" ");
+                for (int i = 0; i < s.length; i++) {
+                    System.out.println(bstobj.get(
+                        Double.parseDouble(s[i])).studentname);
+                }
+            } else if (input[0].equals("GE")) {
+                double max = bstobj.max();
+                String[] s = bstobj.keys(max, Double.parseDouble
+                    (input[1])).toString().split(" ");
+                for (int i = 0; i < s.length; i++) {
+                    System.out.println(bstobj.get(
+                        Double.parseDouble(s[i])).studentname);
+                }
 
+            }
 
-			} else if (input[0].equals("GE")) {
-				double max = bstobj.max();
-				String[] s = bstobj.keys(max, Double.parseDouble
-					(input[1])).toString().split(" ");
-				for (int i = 0; i < s.length; i++) {
-					System.out.println(bstobj.get
-						(Double.parseDouble(s[i])).student_name);
-				}
-
-			}
-
-		}
-	}
+        }
+    }
 }
